8a6f12eac8f709ad66a4bd6edbd5e857517a1050
kyralo <kyralo721@gmail.com>
2019-08-16 21:48:49 +0800

    add header bar

--- src/views/Header/Logo/index.js
+++ src/views/Header/Logo/index.js
@@ -6,8 +6,10 @@ export default class Logo extends React.Component {
     render() {
         return (
             <div className={"_logo"}>
-				<span>歳の翼</span>
-				kyralo
+            	<a href = {"https://kyralo.github.io"}>
+            		<span>歳の翼</span>
+            		<span>kyralo</span>
+            	</a>
 			</div>
         );
     }
