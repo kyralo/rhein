c0f7ed6fec4eaf1061fcd7e46604eb8fd3041266
kyralo <kyralo721@gmail.com>
2020-03-17 14:30:44 +0800

    card component complete

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,45 +1,84 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTags } from '@fortawesome/free-solid-svg-icons'
+
 import "./style.css"
+
+// const card = {
+// 	article_url: '',
+// 	article_img_url: '',
+// 	create_time: '',
+// 	article_title: '',
+// 	type: {
+// 		tags: [
+// 			{
+// 				tag_url: '',
+// 				tag_name: ''
+// 			}
+// 		],
+// 		categories: {
+		// 	categorie_url: '',
+		// 	categorie_name: ''
+		// }
+// 	},
+// 	simple_content: ''
+// }
+// 
+
 
 export default class Card extends React.Component {
 
 	render() {
+
+		const card = this.props.card;
+
 		return (
 			<div className="card">
 				<article className="post post-list-thumb post-list-thumb-left post-list-show" 
 						 itemScope="" itemType="http://schema.org/BlogPosting">
 					  <div className="post-thumb">
-					    <a href="/2018/12/12/Hexo-Theme-Sakura - 副本 (3)/">
+					    <a href={card.article_url}>
 					      <img className="lazyload" 
-					      	   src="https://cdn.jsdelivr.net/gh/honjun/cdn@1.6/img/other/image-404.png" 
+					      	   src={
+					      	   	card.article_img_url == '' 
+					      	   	? 
+					      	   	'https://cdn.jsdelivr.net/gh/honjun/cdn@1.6/img/other/image-404.png'
+					      	   	:
+					      	   	card.article_img_url
+					      	   } 
 					      />
 					    </a>
 					  </div>
 					  <div className="post-content-wrap">
 					    <div className="post-content">
 					      <div className="post-date">
-					        <i className="iconfont icon-time"></i>2018-12-12 22:12
+					        <i className="iconfont icon-time"></i>
+					        {card.create_time}
 					      </div>
-						  <a href="/2018/12/12/Hexo-Theme-Sakura - 副本 (3)/" 
+						  <a href={card.article_url} 
 						  	 className="post-title">
-					        <h3>Hexo-Theme-Sakura</h3>
+					        <h3>{card.article_title}</h3>
 					      </a>
 					      <div className="post-meta">
 					        <span>
-					        	<i className="fa fa-tags"></i>
-					            <a href="/tags/web/">web<em>·</em></a>
-					            <a href="/tags/悦读/">悦读<em>·</em></a>
+					        	<FontAwesomeIcon className="fa fa-tags" icon={faTags} />
+								{
+									card.type.tags.map( (item, key) => {
+										return <a href={item.tag_url} key={key}>{item.tag_name}<em>·</em></a>
+									})
+								}
 					        </span>
 					        <span>
 					          	<i className="iconfont icon-file"></i>
-					           	<a href="/categories/技术/">技术</a>
+
+					           	<a href={card.type.categorie.categorie_url}>{card.type.categorie.categorie_name}</a>
 					        </span>
 					      </div>
 					      <div className="float-content">
-					        <p>hexo-sakura主题使用教程</p>
+					        <p>{card.simple_content}</p>
 					        <div className="post-bottom">
-					          	<a href="/2018/12/12/Hexo-Theme-Sakura - 副本 (3)/" 
+					          	<a href={card.article_url} 
 					          	className="button-normal">
 					            	<i className="iconfont icon-caidan"></i>
 					          	</a>
--- src/views/Body/index.js
+++ src/views/Body/index.js
@@ -12,7 +12,31 @@ const options = {
 					url: 'https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4'
 				},
 				screenshot: 'true'
+			};
+
+const card = {
+	article_url: '/2018/12/12/Hexo-Theme-Sakura - 副本 (3)/',
+	article_img_url: '',
+	create_time: '2018-12-12 22:12',
+	article_title: 'Hexo-Theme-Sakura',
+	type: {
+		tags: [
+			{
+				tag_url: '/tags/web/',
+				tag_name: 'web'
+			},
+			{
+				tag_url: '/tags/悦读/',
+				tag_name: '悦读'
 			}
+		],
+		categorie: {
+			categorie_url: '/categories/技术/',
+			categorie_name: '技术'
+		}
+	},
+	simple_content: 'hexo-sakura主题使用教程'
+};
 
 export default class Body extends React.Component {
 
@@ -23,11 +47,11 @@ export default class Body extends React.Component {
 				margin: 'auto'
 			}}>
 				<Annunciate/>
-				<Card/>
-				<Card/>
-				<Card/>
-				<Card/>
-				<Card/>
+				<Card card={card}/>
+				<Card card={card}/>
+				<Card card={card}/>
+				<Card card={card}/>
+				<Card card={card}/>
 				<VideoPlayer options={options} />
 			</div>
 		);
